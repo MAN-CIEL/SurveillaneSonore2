bcde54d83c8d6bbf38f8b2cb31451ec7fcf7b1ed
Ilan Manzari <ilanmanzari2607@gmail.com>
2025-03-27 17:01:44 +0100

    Commit #6 : test de l'acquisition du son

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,14 @@ void setup() {
   ecranOled.setFont(ArialMT_Plain_10);
   ecranOled.drawString(0, 20, "Version 2024");
   ecranOled.display();
+  son.Setup();
 }
 
 void loop() {
-  Serial.println("*");
-  delay(300);
+  son.SamplesDmaAcquisition();
+  Serial.print("Moyenne: "); Serial.print(son.niveauSonoreMoyen);
+  Serial.print(" Crête: "); Serial.println(son.niveauSonoreCrete);
+  delay(100);
 }
 
 SSD1306 ecranOled(0x3c, 5, 4);
